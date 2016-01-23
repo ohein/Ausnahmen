b06483891e7d8fd6cd7f7d8277d7816fe7b541fe
ohein <oliver.hein@t-online.de>
2016-01-23 11:14:54 +0100

    fdggf

---
+++ EigeneAusnahme.java
@@ -0,0 +1,13 @@
+public class EigeneAusnahme {
+
+	public static void teste() throws MyException {
+		try {
+			MyException ex = new MyException();
+			throw ex;
+		}
+		catch (MyException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
